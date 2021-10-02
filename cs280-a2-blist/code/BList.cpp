093a09c4bc3e8fd3ddce5e5b6689a87bad4d5fe7
jiankaig <79676173+jiankaig@users.noreply.github.com>
2021-10-03 00:31:29 +0800

    v0.3
    implemented push front

--- cs280-a2-blist/code/BList.cpp
+++ cs280-a2-blist/code/BList.cpp
@@ -36,36 +36,30 @@ template <typename T, unsigned Size>
 void BList<T, Size>::push_front(const T& value){
   State ret = FAIL;
   BNode* ptrNode = head_; //start at head node
-  BNode* ptrPrevNode;
-  BNode* ptrCurrNode;
+  BNode* PrevNode;
   while(ret != SUCCESS){
     //if node got space, assign to head..
-    //printf("count: %u\t size: %u \n", ptrNode->count, Size);
     if(ptrNode->count < static_cast<int>(Size)){
-      //std::cout<<"BREAKPT\n";
-      ptrNode->values[ptrNode->count] = value; // assign value to front, based on count
+      //shift current values up by one position
+      int position = ptrNode->count;
+      for(int i=position-1; i>=0; i--){
+        ptrNode->values[i+1] = ptrNode->values[i];
+      }
+      ptrNode->values[0] = value; //insert value at front of node
       ptrNode->count++; //update count
       stats_.ItemCount++;
       ret = SUCCESS;
       break;
     }
-    else if(ptrNode->prev == NULL){
-      //check prev node if it exists
-      //create a new node and link it with previous
-      ptrCurrNode = ptrNode;
-      ptrNode = ptrNode->prev;
-      //std::cout<<"BREAKPT2\n";
-      ptrNode = new BNode();
-      ptrNode->prev = ptrCurrNode;
-      ptrCurrNode->prev = ptrNode;
-      head_ = ptrNode;
-      
-    }
     else{
-      //prev node exists, move pointer to next node
-      //std::cout<<"BREAKPT3\n";
-      ptrPrevNode = ptrNode;
-      ptrNode = ptrNode->next;
+      //create new node and place in front of head node
+      BNode* NewNode = new BNode();
+      PrevNode = ptrNode;
+      ptrNode = NewNode;
+      PrevNode->prev = NewNode;
+      NewNode->next = PrevNode;
+      head_ = NewNode;
+      stats_.NodeCount++;
     }
   }
   
